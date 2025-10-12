68385ed14b836016f06dab9624db12bd3924f2d3
Hasan Yusuf Khan <hasanrafsun5@gmail.com>
2025-10-12 23:14:42 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>This one is the about page</h1>
+</x-layout>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+        <h1 class="text-amber-300 text-9xl">Hello from the home page.</h1>
+</x-layout>
